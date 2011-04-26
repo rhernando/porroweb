e83820d02ff037c130d30d4f29164535ae5f9aaf
Ruben Hernando <hernando.ruben@gmail.com>
2011-04-26 22:37:39 +0200

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porroweb::Application.config.secret_token = '3778b1e654c407ee421db4356c52af4d174f78c4d23caf73032ca5c8b09519a2addeae15ea776a2e981f9581dd23cba0ee38501ceb951ff06c1ab0f4d69c89c3'
