d5cfa3de49adb9241e1ff0772688b3c7b0a4db8b
Ruben Hernando <rhernando@informa.es>
2011-08-02 18:32:42 +0200

    pagina seleccion

--- app/views/js/scriptSeleccion.js
+++ app/views/js/scriptSeleccion.js
@@ -1,30 +1,56 @@
   $(document).ready(function() {
-    $('.equipo').click(function(){
-        var imagen = $(this).find('img');
-        imagen.click(function(){
-            restaurar(this);
-        });
-
-        $('#seleccion #divequipos').append(imagen);
-    });
+    $('.equipo').click(clickEquipo);
 
     $('#validar').click(function(){
+
         var imgs = $("#seleccion #divequipos").find("img");
-        if (imgs.length != <%= @porra.num_equipos %>)
+        if (imgs.length != <%= @porra.num_equipos %>){
             alert("Debes seleccionar <%= @porra.num_equipos %> equipos");
+            return false;
+        }
 
         var suma_puntos = 0;
         for (var i = 0; i < imgs.length;i++){
             suma_puntos += parseInt(imgs[i].id.substring(imgs[i].id.lastIndexOf("-")+1));
         }
-                alert(suma_puntos);
-        if (suma_puntos > <%= @porra.max_val_equipos %>)
+        if (suma_puntos > <%= @porra.max_val_equipos %>){
             alert("La suma del valor de los equipos no pueden superar los <%= @porra.max_val_equipos %> puntos");
+            return false;
+        }
 
+
+        $.post({
+          url: '/seleccion/create'
+        });
+        return false;
     });
   });
 
   function restaurar(img){
-      var idli = "#li-" + img.id;
-      $(idli).find("p").before(img);
+    var idli = "#li-" + img.id.substring(0,img.id.indexOf("-"));
+
+    var equipos = $("#eqs").text();
+    $("#eqs").text(parseInt(equipos) - 1);
+
+    var puntos = $("#pts").text();
+    var restar = parseInt(img.id.substring(img.id.lastIndexOf("-")+1));
+    $("#pts").text(parseInt(puntos) - restar);
+
+    $(idli).append(img);
+  }
+
+  function clickEquipo(){
+    var imagen = $(this).find('img');
+    imagen.click(function(){
+        restaurar(this);
+    });
+
+    var equipos = $("#eqs").text();
+    $("#eqs").text(parseInt(equipos) + 1);
+
+    var puntos = $("#pts").text();
+    var sumar = parseInt($(imagen).attr('id').substring($(imagen).attr('id').lastIndexOf("-")+1));
+    $("#pts").text(parseInt(puntos) + sumar);
+
+    $('#seleccion #divequipos').append(imagen);
   }
